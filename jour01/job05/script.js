5338b17b5f8405a2e8becdeba53e840d9f87164c
SK4 <valentin.beaussaert@laplateforme.io>
2024-03-25 11:25:48 +0100

    function afficherJoursSemaines that work's !

---
+++ jour01/job05/script.js
@@ -0,0 +1,7 @@
+function afficherJoursSemaines() {
+    let jourSemaines = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"];
+    for (let i = 0; i < jourSemaines.length; i++){
+        console.log(jourSemaines[i]);
+    }
+}
+afficherJoursSemaines();
