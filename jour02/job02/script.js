fe164a901209ff7855fc9398cb0e7ef4e9fddb98
SK4 <valentin.beaussaert@laplateforme.io>
2024-03-26 09:49:39 +0100

    function showhide ()

---
+++ jour02/job02/script.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById("button").addEventListener("click", showhide);
+});
+
+function showhide() {
+    var existingArticle = document.getElementById("new-citation");
+    // Si un nouvel article a déjà été ajouté, le supprimer
+    if (existingArticle) {
+        existingArticle.remove();
+    } else {
+        // Sinon, créer un nouvel article avec la nouvelle citation et l'ajouter au document
+        var newArticle = document.createElement("article");
+        newArticle.id = "new-citation";
+        newArticle.textContent = "L'important n'est pas la chute, mais l'atterrissage.";
+        document.body.appendChild(newArticle); // Ajouter l'article au body du document
+    }
+}
