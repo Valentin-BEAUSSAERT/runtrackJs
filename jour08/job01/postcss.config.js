e9087b50a7d9cd92dd9d71b64146178663847cc9
SK4 <valentin.beaussaert@laplateforme.io>
2024-04-04 10:06:01 +0200

    initialisation tailwind enfin

---
+++ jour08/job01/postcss.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  content: ["./job01/*.html", "./job02/*.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
---
+++ jour08/job01/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./job01/*.html", "./job01/*.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
